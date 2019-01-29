a6b19737f4df8f3d20de653b36d809eecc2e6bf5
Manuel Hintermayr <manuel@hintermayr.net>
2019-01-29 16:29:25 +0000

    added all activities

--- app/src/main/java/isswatcher/manuelweb/at/CurrentLocationActivity.java
+++ app/src/main/java/isswatcher/manuelweb/at/CurrentLocationActivity.java
@@ -12,7 +12,7 @@ import android.view.View;
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class FullscreenActivity extends AppCompatActivity {
+public class CurrentLocationActivity extends AppCompatActivity {
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -87,7 +87,7 @@ public class FullscreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_fullscreen);
+        setContentView(R.layout.activity_current_location);
 
         mVisible = true;
         mControlsView = findViewById(R.id.fullscreen_content_controls);
