e7813f4be75af8c4383bc65cef1234f11256c0d2
Manuel Hintermayr <manuel@hintermayr.net>
2019-01-29 17:02:43 +0000

    added button to open another activity

--- app/src/main/java/isswatcher/manuelweb/at/LaunchActivity.java
+++ app/src/main/java/isswatcher/manuelweb/at/LaunchActivity.java
@@ -1,18 +1,23 @@
 package isswatcher.manuelweb.at;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class LaunchActivity extends AppCompatActivity {
+    private Button button;
+
+
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -106,6 +111,12 @@ public class LaunchActivity extends AppCompatActivity {
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
         findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+    }
+
+    public void openMap(View v)
+    {
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
     }
 
     @Override
